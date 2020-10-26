364ba0dc87b0625c376608c40b5ece1ac00b91ef
Casper Boone <mail@casperboone.nl>
2020-10-26 17:18:15 +0100

    Add transformer dsl

---
+++ src/main/kotlin/io/testaxis/backend/http/transformers/dsl/TransformerDsl.kt
@@ -0,0 +1,30 @@
+package io.testaxis.backend.http.transformers.dsl
+
+typealias KeyValueData = Map<String, Any?>
+typealias converter<T> = (T) -> KeyValueData
+
+abstract class Transformer {
+    private var data = mutableMapOf<String, Any?>()
+
+    protected infix operator fun <T> String.minus(value: T) {
+        data[this] = value
+    }
+
+    protected fun <T> transform(
+        entity: T,
+        vararg extendConverters: converter<T>,
+        convertData: T.() -> Unit
+    ): KeyValueData {
+        val extendedConvertersData = extendConverters.fold(mapOf<String, Any?>()) { data, convert ->
+            data + convert(entity)
+        }
+
+        data = mutableMapOf()
+        entity.convertData()
+
+        return extendedConvertersData + data
+    }
+
+    fun <T> transform(entities: List<T>, converter: converter<T>) = entities.map(converter)
+}
+
