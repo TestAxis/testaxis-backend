4bb190b56f848809acb8fec397a4523a7026b9ef
Casper Boone <mail@casperboone.nl>
2020-10-28 11:47:02 +0100

    Refactor to JPA compatible models

--- src/main/kotlin/io/testaxis/backend/models/Project.kt
+++ src/main/kotlin/io/testaxis/backend/models/Project.kt
@@ -3,20 +3,16 @@ package io.testaxis.backend.models
 import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.Entity
 import javax.persistence.FetchType
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.OneToMany
 
 @Entity
-data class Project(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
-    val slug: String,
-    val name: String,
+class Project(
+    var slug: String,
+    var name: String,
     @JsonIgnore @OneToMany(fetch = FetchType.LAZY) @JoinColumn(name = "project_id")
-    val builds: List<Build>? = null,
-) {
+    var builds: MutableList<Build>? = null,
+) : AbstractJpaPersistable<Long>() {
     companion object {
         fun splitNameFromSlug(slug: String) = slug.split('/')[0]
     }
--- src/main/kotlin/io/testaxis/backend/models/TestCaseExecution.kt
+++ src/main/kotlin/io/testaxis/backend/models/TestCaseExecution.kt
@@ -4,26 +4,22 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.util.Date
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.Lob
 import javax.persistence.ManyToOne
 
 @Entity
-data class TestCaseExecution(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
-    @ManyToOne @JoinColumn(name = "build_id") val build: Build,
-    val testSuiteName: String,
-    val name: String,
-    val className: String,
-    val time: Double,
-    val passed: Boolean,
-    @Lob val failureMessage: String?,
-    val failureType: String?,
-    @Lob val failureContent: String?,
-    @CreatedDate val createdAt: Date = Date(),
+class TestCaseExecution(
+    @ManyToOne @JoinColumn(name = "build_id") var build: Build,
+    var testSuiteName: String,
+    var name: String,
+    var className: String,
+    var time: Double,
+    var passed: Boolean,
+    @Lob var failureMessage: String?,
+    var failureType: String?,
+    @Lob var failureContent: String?,
+    @CreatedDate var createdAt: Date = Date(),
     @Suppress("ForbiddenComment") // TODO: Fix @CreatedDate and @LastModifiedDate annotations
-    @LastModifiedDate val updatedAt: Date = Date(),
-)
+    @LastModifiedDate var updatedAt: Date = Date(),
+) : AbstractJpaPersistable<Long>()
