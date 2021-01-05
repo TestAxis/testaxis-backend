dd59048fb5496749f703bca27740734fda79d45f
Casper Boone <mail@casperboone.nl>
2021-01-05 16:46:03 +0100

    Show token after social login

--- src/main/kotlin/io/testaxis/backend/http/controllers/auth/AuthController.kt
+++ src/main/kotlin/io/testaxis/backend/http/controllers/auth/AuthController.kt
@@ -12,10 +12,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 import javax.validation.Valid
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
@@ -55,6 +58,9 @@ class AuthController(
             )
         )
     }
+
+    @GetMapping("/token")
+    fun displayToken(@RequestParam token: String) = ModelAndView("token", mapOf("token" to token))
 }
 
 data class LoginRequest(
