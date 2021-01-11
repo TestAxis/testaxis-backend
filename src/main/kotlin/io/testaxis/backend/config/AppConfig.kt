871ab54a3e06f3694d2828effcb491682a56cb49
Casper Boone <mail@casperboone.nl>
2021-01-11 16:10:44 +0100

    Increase allowed validation file size

--- src/main/kotlin/io/testaxis/backend/config/AppConfig.kt
+++ src/main/kotlin/io/testaxis/backend/config/AppConfig.kt
@@ -4,5 +4,5 @@ object AppConfig {
     /**
      * The file size limit for uploaded files (in bytes).
      */
-    const val UPLOAD_LIMIT: Long = 10 * 1000 * 1000
+    const val UPLOAD_LIMIT: Long = 99 * 1000 * 1000 // 99 MB
 }
