945f27a5091c98aeda56dd8797875b4198940e30
Casper Boone <mail@casperboone.nl>
2021-02-09 14:24:23 +0100

    Add "show all builds" endpoint

--- src/main/kotlin/io/testaxis/backend/http/controllers/ExperimentController.kt
+++ src/main/kotlin/io/testaxis/backend/http/controllers/ExperimentController.kt
@@ -10,14 +10,15 @@ import org.springframework.context.ApplicationEventPublisher
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.Date
 
 @RestController
 class ExperimentController(val projectRepository: ProjectRepository, val buildRepository: BuildRepository, val applicationEventPublisher: ApplicationEventPublisher) {
     /**
      * Hides all builds that are not on the main branch of the JPacman project.
      */
-    @GetMapping("/experiment/hide")
-    fun hideBuilds() {
+    @GetMapping("/experiment/hide-all")
+    fun hideAllBuilds() {
         projectRepository.findBySlug("testaxis/jpacman")
             ?.builds
             ?.filter { it.branch != "main" }
@@ -28,11 +29,25 @@ class ExperimentController(val projectRepository: ProjectRepository, val buildRe
     }
 
     /**
+     * Show all builds of the JPacman project.
+     */
+    @GetMapping("/experiment/show-all")
+    fun showAllBuilds() {
+        projectRepository.findBySlug("testaxis/jpacman")
+            ?.builds
+            ?.forEach {
+                it.visible = true
+                buildRepository.save(it)
+            }
+    }
+
+    /**
      * Shows a given build and publishes a new event to announce it.
      */
     @GetMapping("/experiment/show/{build}")
     fun showBuild(@PathVariable @MustExist build: Build) {
         build.visible = true
+        build.createdAt = Date()
         val persistedBuild = buildRepository.save(build)
 
         applicationEventPublisher.publishEvent(BuildWasCreatedEvent(this, persistedBuild))
